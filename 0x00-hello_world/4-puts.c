f963f0e8f2efc98ecb965cab39e46050d130b6d1
Debbie <debyovwin@gmail.com>
2023-03-16 15:48:50 -0700

    fixing error 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /*
  *This is how to use puts
- *and also make the code return a value to show success
- *Main is the funtion where puts is executed
+ *and also make the code return a value 0 which shows 
+ * the code ran successfully
+ *Main - the funtion where puts is executed
  *
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
