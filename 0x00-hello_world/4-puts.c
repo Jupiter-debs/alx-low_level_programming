aed06cb83f3c0c0c293d11e76f64b9746c22c81e
Debbie <debyovwin@gmail.com>
2023-03-16 15:42:30 -0700

    writing c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *This is how to use puts
+ *and also make the code return a value to show success
+ *Main is the funtion where puts is executed
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
