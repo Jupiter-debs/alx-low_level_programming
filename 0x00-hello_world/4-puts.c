90ccb3f57b8f135849f724cfb0655d5972f154ff
Debbie <debyovwin@gmail.com>
2023-03-16 17:32:14 -0700

    Fixed all errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-/*
- *main - funtion to run puts 
+/**
+ * main - funtion to run put
  *
- *
- *return - returns (0) when successful
- *
- *
- *
+ * Return: returns (0) when successful
  */
 int main(void)
 {
+	/* this displays the text in the put function */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
