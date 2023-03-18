1de16ee3a8e9692eebcdf66c361e225e55d98070
Debbie <debyovwin@gmail.com>
2023-03-17 17:19:24 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+	char c = 97;
+
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+
+	return (0);
+}
