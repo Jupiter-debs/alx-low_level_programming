abedcd022926c7fda90bcdb1b68f147a2da422ec
Debbie <debyovwin@gmail.com>
2023-03-17 18:29:55 -0700

    Sixth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:  0 if success
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
