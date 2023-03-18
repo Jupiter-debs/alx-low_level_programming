106708dbea23d7a0de3ee6a08ed330d8b4961e80
Debbie <debyovwin@gmail.com>
2023-03-17 19:45:14 -0700

    nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,3 +6,19 @@
  */
 
 int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(44);
+			putchar(32);
+			putchar(j);
+		}
+	}
+	return (0);
+}
