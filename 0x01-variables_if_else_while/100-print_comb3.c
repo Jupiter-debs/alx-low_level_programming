40cdbe5eeb7f849166630bacc6de37264572d33e
Debbie <debyovwin@gmail.com>
2023-03-18 07:14:58 -0700

    try 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:  0 if success
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (ij == ji)
+			{
+				continue;
+			}
+			if (i == j && j == i)
+			{
+				continue;
+			}
+			if (ij > ji)
+			{
+				continue;
+			}
+			putchar (i);
+			putchar(j);
+			putchar(44);
+			putchar(32);
+			i++;
+			j++;
+		}
+	}
+	return (0);
